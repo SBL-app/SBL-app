3f6574e9a4ea152abad57d4f167aee9a2b393f76
galexand <giordana.alex@gmail.com>
2024-06-27 16:56:20 +0200

    fix UI bugs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup>
 import TheNavbar from './components/TheNavbar.vue'
-import IncomingEvents from './components/IncomingEvents.vue'
-import TheSeasons from './components/TheSeasons.vue'
-import ThePresentation from './components/ThePresentation.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <TheNavbar />
-  <IncomingEvents />
-  <TheSeasons />
-  <ThePresentation />
+  <router-view />
 </template>
 
 <style scoped></style>
