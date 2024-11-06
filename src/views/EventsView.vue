326159204624774749f01a00d5f6b6361262d44c
galexand <giordana.alex@gmail.com>
2024-11-06 17:24:50 +0100

    now import the component IncomingEvent

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,63 +1,9 @@
 <script setup>
-import { RouterLink } from "vue-router";
+import IncomingEvents from "../components/IncomingEvents.vue";
 </script>
 <template>
   <div class="event-focus">
-    <div class="event-container">
-      <p class="title">Prochains évènements :</p>
-      <router-link to="/event/2" class="event-item">
-        <p id="season-title">Saison 2</p>
-        <p id="team-number">10 équipes</p>
-        <div class="dates">
-          <p id="start-date">01/01</p>
-          <div class="arrow">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="41"
-              height="8"
-              viewBox="0 0 41 8"
-              fill="none"
-            >
-              <path
-                d="M40.5278 4.35355C40.7231 4.15829 40.7231 3.84171 40.5278 3.64645L37.3459 0.464466C37.1506 0.269204 36.834 0.269204 36.6387 0.464466C36.4435 0.659728 36.4435 0.976311 36.6387 1.17157L39.4672 4L36.6387 6.82843C36.4435 7.02369 36.4435 7.34027 36.6387 7.53553C36.834 7.7308 37.1506 7.7308 37.3459 7.53553L40.5278 4.35355ZM0.174278 4.5L40.1743 4.5L40.1743 3.5L0.174278 3.5L0.174278 4.5Z"
-                fill="white"
-              />
-            </svg>
-          </div>
-          <p id="end-date">20/01</p>
-        </div>
-        <div class="inscription-status">
-          <p id="inscription-title">inscription :</p>
-          <p id="inscription-status">terminé</p>
-        </div>
-      </router-link>
-      <router-link to="/event/3" class="event-item">
-        <p id="season-title">Saison 3</p>
-        <p id="team-number">13 équipes</p>
-        <div class="dates">
-          <p id="start-date">01/01</p>
-          <div class="arrow">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="41"
-              height="8"
-              viewBox="0 0 41 8"
-              fill="none"
-            >
-              <path
-                d="M40.5278 4.35355C40.7231 4.15829 40.7231 3.84171 40.5278 3.64645L37.3459 0.464466C37.1506 0.269204 36.834 0.269204 36.6387 0.464466C36.4435 0.659728 36.4435 0.976311 36.6387 1.17157L39.4672 4L36.6387 6.82843C36.4435 7.02369 36.4435 7.34027 36.6387 7.53553C36.834 7.7308 37.1506 7.7308 37.3459 7.53553L40.5278 4.35355ZM0.174278 4.5L40.1743 4.5L40.1743 3.5L0.174278 3.5L0.174278 4.5Z"
-                fill="white"
-              />
-            </svg>
-          </div>
-          <p id="end-date">20/01</p>
-        </div>
-        <div class="inscription-status">
-          <p id="inscription-title">inscription :</p>
-          <p id="inscription-status">en cours</p>
-        </div>
-      </router-link>
-    </div>
+    <IncomingEvents />
   </div>
 </template>
 <style scoped>
